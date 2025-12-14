4ae6b4abc38e2880874072bf0c8a8b8bddf19393
lqzh <9134671+lqzhgood@users.noreply.github.com>
2025-12-14 21:06:56 +0800

    🎈 perf: 补充内容

--- docs/.vitepress/configs/head.ts
+++ docs/.vitepress/configs/head.ts
@@ -1,7 +1,7 @@
-import { withBase, type HeadConfig } from 'vitepress';
+import { type HeadConfig } from 'vitepress';
 
 // const isDevelopment = process.env.NODE_ENV === 'development';
 
 export const head: HeadConfig[] = [
-    ['link', { rel: 'icon', href: withBase('/favicon.ico') }],
+    ['link', { rel: 'icon', href: '/favicon.ico' }],
 ];
